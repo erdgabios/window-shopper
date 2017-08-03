9d87f06aab614d5a57e040218cb3015337afce42
Gabriel Erdelyi <erdgabios@gmail.com>
2017-08-03 15:59:17 +0200

    Input accesory views & IBDesignable

--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,31 +8,28 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+        
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
-        
+            
+            
         if let p = placeholder {
-            let place = NSMutableAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
-            attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            let place = NSMutableAttributedString(string:p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
+                attributedPlaceholder = place
+                textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        
-        
-        
     }
-    
-    
-    
-    
-    
-    
-    
-    
 }
